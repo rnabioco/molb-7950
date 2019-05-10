3dea4b8d9f8c29d3fefd78473cc3972f136a82b3
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-05-10 12:40:39 -0600

    Add qpcr data

---
+++ R/MOLB7950-package.R
@@ -0,0 +1,9 @@
+#' Informatics and Statistics for Molecular Biologists (MOLB 7950)
+#'
+#' @docType package
+#' @name MOLB7950
+#'
+#' @import tidyverse
+#' @import ggrepel
+#' @import gapminder
+"_PACKAGE"
