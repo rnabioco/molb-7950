41b98e96e34f0851a4025caa054a6c093d6593fb
Jay Hesselberth <jay.hesselberth@cuanschutz.edu>
2023-05-28 06:38:18 -0600

    base quarto site

--- R/MOLB7950-package.R
+++
@@ -1,9 +0,0 @@
-#' Informatics and Statistics for Molecular Biologists (MOLB 7950)
-#'
-#' @docType package
-#' @name MOLB7950
-#'
-#' @import tidyverse
-#' @import ggrepel
-#' @import gapminder
-"_PACKAGE"
