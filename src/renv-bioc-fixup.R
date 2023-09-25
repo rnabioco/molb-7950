2a8f206ead8efc98068a78cc2fac98a920ddecb5
Jay Hesselberth <jay.hesselberth@cuanschutz.edu>
2023-09-25 12:48:32 -0600

    fixup fixup script [ci skip]

--- src/renv-bioc-fixup.R
+++ src/renv-bioc-fixup.R
@@ -1,28 +1,37 @@
 # fix up bioconductor sources in renv.lock
+#
+# for some reason, some Bioconductor packages end up with Source: "Repository"
+# and an undefined "Repository" field.
+# 
+# this just identifies those packages, sets Source: "Bioconductor", and then
+# re-writes the lock file
+
 library(jsonlite)
 library(tidyverse)
 library(renv)
 
-renv_json <- jsonlite::read_json("renv.lock")
-
 wrong_source <- function(x) {
   x$Source == "Repository" && is.null(x$Repository)
 }
-
 fix_source <- function(x) {
   x$Source <- "Bioconductor"
   x
 }
 
+renv_json <- jsonlite::read_json("renv.lock")
 pkgs <- renv_json$Packages
 
-fix_idx <- purrr::map_lgl(
-  pkgs,
-  ~ wrong_source(.x)
-) |> which()
-
-pkgs_fix <- purrr::map_at(pkgs, fix_idx, fix_source)
+pkgs_fix <-
+  purrr::map_lgl(
+    pkgs,
+    ~ wrong_source(.x)
+  ) |> 
+  which() |>
+  purrr::map_at(
+    .x = pkgs,
+    .at = _,
+    .f = fix_source
+  )
 
 renv_json$Packages <- pkgs_fix
-
 renv:::renv_json_write(renv_json, file = "renv.lock")
