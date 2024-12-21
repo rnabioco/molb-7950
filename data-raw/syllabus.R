4c3bb24fdf21c2348dca7f135ab0143f30809788
Jay Hesselberth <jay.hesselberth@cuanschutz.edu>
2024-12-21 16:48:44 -0700

    Upkeep Fall 2025 (#35)

--- data-raw/syllabus.R
+++ data-raw/syllabus.R
@@ -3,7 +3,7 @@ library(here)
 library(readr)
 
 # contact jay if you need access to the class sheet below
-ss <- "https://docs.google.com/spreadsheets/d/1MSu1YZdKk7LK9-m7EjzoMWggwlsEJ7dC1aiax85uvrE/edit#gid=1069962431"
+ss <- "https://docs.google.com/spreadsheets/d/1eqB6kuWEExhSotdtF5uDU8nKpNlME2BQH5dw1EhAlH8/edit?gid=1069962431#gid=1069962431"
 
 sched_tbl <- read_sheet(ss, sheet = "Syllabus") |>
   write_tsv(here("data/syllabus.tsv"))
